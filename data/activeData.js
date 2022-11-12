b82e3c92f557d15eb291244df257fce479d3a74e
Germán <111938844+GermanS04@users.noreply.github.com>
2022-11-12 06:30:14 -0600

    Add files via upload

---
+++ data/activeData.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        "clientID":12,
+        "clientName":"Julio Cesar",
+        "phone":"+52 57914063",
+        "addres":"25.650445, -100.289791",
+        "safeSpace":"Neighbour.",
+        "delivered":0,
+    },
+    {
+        "clientID":26,
+        "clientName":"Jose Luis Diaz",
+        "phone":"+52 432134563",
+        "addres":"434.1343, 123,45654",
+        "safeSpace":"None.",
+        "delivered":0
+    },
+    {
+        "clientID":38,
+        "clientName":"Eliseo Diego",
+        "phone":"+52 46531324",
+        "addres":"Mexico",
+        "safeSpace":"Security Guard",
+        "delivered":0
+    },
+    {
+        "clientID":43,
+        "clientName":"Emiliano Zapata",
+        "phone":"+52 97314702",
+        "addres":"Somewhere",
+        "safeSpace":"Throw to backyard",
+        "delivered":0
+    },
+    {
+        "clientID":15,
+        "clientName":"Richard Anaya",
+        "phone":"+52 16941578",
+        "addres":"Mexico",
+        "safeSpace":"550.445, -10.289791",
+        "delivered":0
+    },
+]
