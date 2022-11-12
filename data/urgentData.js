61019ae904539e698eee4e2bce6a29c85a6d8a2c
Oscar Arreola <77394565+Oscar-gg@users.noreply.github.com>
2022-11-12 02:06:10 -0600

    Add files via upload

---
+++ data/urgentData.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        "clientID":1,
+        "clientName":"Nicolas Treviño",
+        "phone":"+528111901547",
+        "addres":"25.650445, -100.289791",
+        "safeSpace":"Behind the plants.",
+        "delivered":0,
+    },
+    {
+        "clientID":2,
+        "clientName":"Carlos Martinez",
+        "phone":"+52 8111996969",
+        "addres":"America",
+        "safeSpace":"Below the car.",
+        "delivered":0
+    },
+    {
+        "clientID":3,
+        "clientName":"Pepe Martinez",
+        "phone":"+52 12345678",
+        "addres":"Mexico",
+        "safeSpace":"250.650445, -80.289791",
+        "delivered":0
+    },
+    {
+        "clientID":4,
+        "clientName":"Checo Perez",
+        "phone":"+52 9812222204",
+        "addres":"Somewhere",
+        "safeSpace":"Throw to backyard",
+        "delivered":0
+    },
+    {
+        "clientID":5,
+        "clientName":"Canelo Alvarez",
+        "phone":"+52 12345678",
+        "addres":"Mexico",
+        "safeSpace":"550.445, -10.289791",
+        "delivered":0
+    },
+]
