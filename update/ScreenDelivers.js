4cf9ee75f191e2aa62749e4d16baae6b3a309d29
Oscar Arreola <77394565+Oscar-gg@users.noreply.github.com>
2022-11-12 02:09:26 -0600

    Modification of classes to implement FlatList

---
+++ update/ScreenDelivers.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import data from "../data/completedData";
+import RepositoryList from './RepositoryList';
+
+export default function ScreenC({ navigation }) {
+    return (
+        
+        <View >
+            <View style={styles.topNoch}>
+                <Text style={styles.sectionTittle}> Delivered</Text>
+            </View>
+            <RepositoryList {...data} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 50,
+    },
+    topNoch: {
+        backgroundColor: '#4c9ccf',
+        height: 140,
+        borderRadius: 20,
+    },
+    sectionTittle: {
+
+        color: '#fff',
+        paddingLeft: 20,
+        paddingTop: 80,
+        fontSize: 24,
+        fontWeight: 'bold',
+
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#DADAE6',
+      },
+
+});
+
---
+++ update/ScreenUrgents.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import RepositoryListUrgent from './RepositoryListUrgent';
+import data from "../data/urgentData";
+
+export default function ScreenB({ navigation }) {
+    return (
+        <View >
+            <View style={styles.topNoch}>
+                <Text style={styles.sectionTittle}> Urgent Deliveries</Text>
+            </View>
+            <RepositoryListUrgent {...data} />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    body: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 50,
+    },
+    topNoch: {
+        backgroundColor: '#4c9ccf',
+        height: 140,
+        borderRadius: 20,
+    },
+    sectionTittle: {
+
+        color: '#fff',
+        paddingLeft: 20,
+        paddingTop: 80,
+        fontSize: 24,
+        fontWeight: 'bold',
+
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#DADAE6',
+      },
+
+});
+
+
