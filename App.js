025eb2d64d1d7b9c4f188a70315f5b8b092cec9f
Germán <111938844+GermanS04@users.noreply.github.com>
2022-11-11 19:47:26 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React,{useState} from 'react';
 import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import Urgents from './src/ScreenUrgents';
 import Orders from './src/ScreenOrders';
 import Delivers from './src/ScreenDelivers';
+
+//for navigation use [npm install @react-navigation/native] in terminal
+import { NavigationContainer } from '@react-navigation/native';
+
+//for bottomtab use [npm install @react-navigation/bottom-tabs] in terminal
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
